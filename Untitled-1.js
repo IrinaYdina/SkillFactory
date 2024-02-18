1a0f5adcddee50c70393047edeb236ec17977244
YdinaIrina <ydinaira7704@gmail.com>
2024-02-18 16:32:31 +0300

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,17 @@
+
+var a = ["a", "b", "c", "f", "egfuy3gf"];
+var index;
+for (index = a.length - 1; index >= 0; --index) {
+    console.log(a[index]);
+}
+console.log(a.length);
+
+
+
+
+
+
+
+
+
+
